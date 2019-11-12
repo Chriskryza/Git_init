82e108c838c8a6b5ee4ea5645e6d15dd6cd474ba
Chriskryza <krauze.krzysztof@wp.pl>
2019-11-12 20:12:53 +0100

    zmiana

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hallo World");
         System.out.println("publikujemy pierszy program!!!");
-
+        System.out.println("kolejna zmiana!!!");
+        System.out.println("Edycja przez współpracownika"
     }
 }
